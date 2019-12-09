1494289308cbdd588ea9b1ac01e50b2de00b879f
dmitry-mag <mdvaven@mail.ru>
2019-12-10 00:17:15 +0300

    add folium to requirements, made map on base page

--- webapp/app.py
+++ webapp/app.py
@@ -1,9 +1,19 @@
 from flask import Flask
+import folium
+
+
+START_POSITION = [44.4, 39.75]
 
 
 app = Flask(__name__)
+map = folium.Map(location=START_POSITION, zoom_start=9)
+folium.Marker(location=START_POSITION).add_to(map)
 
 
 @app.route('/')
 def hello():
-        return 'Hello, geo-treasure!'
+        return map._repr_html_()
+
+
+if __name__ == '__main__':
+        app.run(debug=True)
