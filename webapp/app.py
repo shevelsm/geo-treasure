c139e044d6d8da9b19ad434508ca638c6179b5c4
a.shevelev <shevelsm@gmail.com>
2019-12-10 16:56:17 +0300

    add initial  index.html and style.css

--- webapp/app.py
+++ webapp/app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
-
+bootstrap = Bootstrap(app)
 
 @app.route('/')
-def hello():
-        return 'Hello, geo-treasure!'
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
