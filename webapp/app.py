a50342608dd736d1baf1db1e884891bec58066ad
Dmitry Magnich <mdvaven@mail.ru>
2019-12-11 17:53:44 +0300

    finish data generator, place markers from data on map, need test

--- webapp/app.py
+++ webapp/app.py
@@ -1,19 +1,29 @@
 from flask import Flask
 import folium
-
-
-START_POSITION = [44.4, 39.75]
+import json
+import os
 
 
 app = Flask(__name__)
-map = folium.Map(location=START_POSITION, zoom_start=9)
-folium.Marker(location=START_POSITION).add_to(map)
 
+
+# in future it will be algorithm
+def markers_generator(range=10):
+    """ range - searching radius for places """
+    with open(os.path.join('data', 'ready50dots.json'), 'r', encoding='utf-8') as file:
+        markers_data = json.loads(file.read())
+    return markers_data
 
 @app.route('/')
-def hello():
-        return map._repr_html_()
+def index():
+    # in future get start_position as argument
+    start_position = [44.4, 39.75]
+    folium_map = folium.Map(location=start_position, zoom_start=9)
+    for marker in markers_generator():
+        folium.Marker([marker['y'], marker['x']], popup=marker['description'],
+                        tooltip='Click for more').add_to(folium_map)
+    return folium_map._repr_html_()
 
 
 if __name__ == '__main__':
-        app.run(debug=True)
+    app.run(debug=True)
--- webapp/data/data_generator.py
+++ webapp/data/data_generator.py
@@ -9,8 +9,8 @@ SOURCE_RANGE = (0, 3)
 DESCRIPTION = 'some html description here'
 
 # square area for marker generation in 4 digits, where xxxx = XX.XX0000 degree
-X_RANGE = (3900, 4100)
-Y_RANGE = (4400, 4500)
+X_RANGE = (3850, 4100)
+Y_RANGE = (4330, 4550)
 
 
 # collect arguments from user
@@ -21,24 +21,30 @@ while True:
         continue
     try:
         number_of_dots = int(input('Enter number of dots to generate: '))
-    except TypeError:
+    except ValueError:
         print('Please, enter digits number of dots!')
         continue
     file_name = input('Enter file name for json data file: ')
-    if not file_name.isalpha():
-        print('Enter valid file name with only text symbols!')
+    if not file_name.isalnum():
+        print('Enter valid file name with only text and digit symbols!')
         continue
     break
 
 
-# generate json data file
+# generate python data
 data = []
 for dot in range(number_of_dots):
     current_dot = {}
-    current_dot['x'] = randrange(*X_RANGE)
-    current_dot['y'] = randrange(*Y_RANGE)
-    print(current_dot)
+    current_dot['x'] = randrange(X_RANGE[0] * 10**4, X_RANGE[1] * 10**4) / 10**6
+    current_dot['y'] = randrange(Y_RANGE[0] * 10**4, Y_RANGE[1] * 10**4) / 10**6
+    current_dot['description'] = DESCRIPTION
+    if data_type == 's':
+        current_dot['source_id'] = randrange(*SOURCE_RANGE)
+        current_dot['rank'] = randrange(*RANK_RANGE)
+    data.append(current_dot)
 
 
-
-# with open(file_name + '.json', 'w', encoding='utf-8') as file:
+# write python data to json file
+json_data = json.dumps(data, indent=4)
+with open(file_name + '.json', 'w', encoding='utf-8') as file:
+    file.write(json_data)
