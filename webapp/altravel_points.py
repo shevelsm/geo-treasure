1d18b208945c8fc12b84e1ae83891256d4a6c8a6
dmitry-mag <mdvaven@mail.ru>
2019-12-21 13:04:26 +0300

    replace and rename altravel_pionts

--- webapp/altravel_points.py
+++ webapp/altravel_points.py
@@ -1,5 +1,4 @@
 import requests
-import os
 from bs4 import BeautifulSoup
 from webapp.app import db, app
 from webapp.model import Point
@@ -49,7 +48,7 @@ def save_info_to_bd(title, source, url, lat, long, info):
         db.session.commit()
 
 
-if __name__ == '__main__':
+def get_altravel_points():
     region_url = 'https://altertravel.ru/catalog/Краснодарский%20край'
     pages_id = get_pages_id(region_url)
     with app.app_context():
