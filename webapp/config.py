5570fd97bcebb88e6cfff18a3093934a3726212b
a.shevelev <shevelsm@gmail.com>
2019-12-27 23:24:37 +0300

    in work Creating an Assotiation table in the db

--- webapp/config.py
+++ webapp/config.py
@@ -2,3 +2,5 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "..", "webapp.db")
+
+SECRET_KEY = "lkdjap;oij;,wd0023)()(2n2n)lkdqwoeqo34>o3k5%"
