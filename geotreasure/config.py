7f7b0b08c184d708a25ee104b100c2f57d24eb80
Aleksei Shevelev <34895456+Shevelsm@users.noreply.github.com>
2020-02-22 11:35:11 +0300

    remove obsolete data

--- geotreasure/config.py
+++ geotreasure/config.py
@@ -3,5 +3,3 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "..", "geotreasure.db")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-SECRET_KEY = "&,aowbp#X*%!+CiwMD<@X1Z`'<S0\i$"
